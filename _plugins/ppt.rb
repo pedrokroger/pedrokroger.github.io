a0ee2fa27516d212877048c3e2bb0dee5879bc55
Pedro Kroger <kroger@pedrokroger.net>
2024-04-10 20:14:21 -0300

    Shortcut to powerpoint icon

---
+++ _plugins/ppt.rb
@@ -0,0 +1,11 @@
+class DownloadInlineTag < Liquid::Tag
+  def initialize(tag_name, input, tokens)
+    super
+  end
+
+  def render(context)
+    return  '<i class="bi bi-file-earmark-ppt-fill" style="font-size: 1.2rem; color: #eb34d2;"></i>'
+  end
+end
+
+Liquid::Template.register_tag('ppt', DownloadInlineTag)
