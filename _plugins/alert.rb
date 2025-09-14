cb18b3628de660e5c41df9bd89e0e65469066867
Pedro Kroger <kroger@ufba.br>
2025-09-14 19:47:10 -0300

    Update alert

--- _plugins/alert.rb
+++ _plugins/alert.rb
@@ -3,7 +3,7 @@ module Jekyll
 
     def render(context)
       text = super
-      "<p class='alert alert-warning'>#{text}</p>"
+      "<p class='alert alert-warning' role='alert'>#{text}</p>"
     end
 
   end
